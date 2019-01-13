8d94471a34dbe8c3e381426920c65c35dfbc106a
Sachin PC <sachinpc.1993@lie.com>
2019-01-13 23:23:45 +0530

    Initial Commit with basic features
    - server.R file
    - ui.r file
    - Initial ui with navigation bar panels
    - Basic exploration of Bus routes involving - Distance, Duration, Type of bus route

---
+++ resources/App/BMTC/ui.R
@@ -0,0 +1,61 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(navbarPage("BMTC Exploratory Data Analysis",
+                             tabPanel("Home",
+                                      fluidRow(
+                                        HTML("<center><h2>Schema Analysis</h2></center>")
+                                        ),
+                                      fluidRow(
+                                        sidebarLayout(
+                                          sidebarPanel(
+                                            uiOutput("columnSelection")
+                                          ),
+                                          mainPanel(
+                                            HTML("TODO Viz here")
+                                          )
+                                        )
+                                      )
+                                      ),
+                             tabPanel("Bus Routes",
+                                      fluidRow(
+                                        sidebarLayout(
+                                          sidebarPanel(
+                                            sliderInput("slider_RouteDistance",
+                                                        label = h4("Route Distance"), 
+                                                        min = 0, 
+                                                        max = 100, 
+                                                        value = c(0,30)
+                                            ),
+                                            uiOutput("slider_duration")
+                                            ),
+                                          mainPanel(
+                                            tabsetPanel(
+                                              tabPanel("Trip Type Frequency", 
+                                                       plotOutput("roundTripFreq")
+                                                       ),
+                                              tabPanel("Distance Vs Duration", plotOutput("distanceDurationScatter"))
+                                              )
+                                            )
+                                          )
+                                        ),
+                                      fluidRow(
+                                        dataTableOutput("tripFreqDatatable")
+                                      )
+                                      ),
+                             tabPanel(
+                               "Dataset Summary",
+                                      fluidRow(
+                                                HTML("<center><h2>Routes Data</h2></center>"),
+                                                verbatimTextOutput("busRoutesDataSummary")
+                                                )))
+))
